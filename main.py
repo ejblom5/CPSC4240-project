cb0e99a0be852a7529bc891bafdab120b95ad3a2
senewco <senewco@clemson.edu>
2021-03-30 12:48:30 -0400

    added 3)

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 import subprocess
 import os
 
-title='Intuitive Network Analysis Tool'
+title='\nIntuitive Network Analysis Tool'
 print(title)
 
 def install_assets():
-    os.system("sudo apt-get install nload")
-    os.system("sudo apt-get install nmap")
-def network_data_usage():
-    os.system("nload")
+	os.system("sudo apt-get install nload")
+	os.system("sudo apt-get install nmap")
 
 def list_devices():
-    print("thinking...")
-    cmd = "hostname -I"
-    ip = os.popen(cmd).read().split(' ')[0]
-    os.system("sudo nmap "+ip+"/24 | grep MAC")
+	print("thinking...")
+	cmd = "hostname -I"
+	ip = os.popen(cmd).read().split(' ')[0]
+	os.system("sudo nmap "+ip+"/24 | grep MAC")
+
+def network_data_usage():
+	os.system("nload")
+
+def list_ports():
+	os.system("ss -a -t | less")
 
 response = ""
 while(response != "quit"):
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("What would you like to do?")
-    print("0) Install required assets")
-    print("1) List devices on your network")
-    print("2) Show network data usage for your computer (use CTRL+C to exit)")
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-    response = input("Choose an option from the list above or enter 'quit' to exit: ")
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	print("What would you like to do?\n")
+	print("0) Install required assets.")
+	print("1) List devices on your network.")
+	print("2) Show network data usage for your computer.")
+	print("3) Show active network ports for your computer.")
+	print("\nUse CTRL+C or type 'quit' to exit.")
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+	response = input("Choose an option from the list above:\n")
 
-    if response == "0":
-        install_assets()
-    if response == "1":
-        list_devices()
-    if response == "2":
-        network_data_usage()
+	if response == "0":
+		install_assets()
+
+	if response == "1":
+		list_devices()
+
+	if response == "2":
+		network_data_usage()
+
+	if response == "3":
+		list_ports()
+
+	os.system("clear")
+
+
+
