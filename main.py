e6f131cd94c367a3674559e5c57a7e986d41cbc7
erik <erik.blom@eleostech.com>
2021-04-05 17:49:21 -0400

    start/stop networking

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ def install_assets():
     os.system("sudo apt-get install nload")
     os.system("sudo apt-get install nmap")
     os.system("sudo apt install inetutils-traceroute")
+    os.system("sudo apt install ethtool")
 
 def path_time_to_website():
-    website = input("Please enter a valid website: \n")
+    website = input("Please enter a valid website (ex. www.google.com) : \n")
     os.system("traceroute "+website)
 
 def shut_down_all_networking():
-    os.system("/etc/init.d/network stop")
+    print(">> Shutting down networking interface...")
+    os.system("systemctl stop NetworkManager")
 
 def start_up_all_networking():
-    os.system("/etc/init.d/network start")
+    print(">> Starting up networking interface...")
+    os.system("systemctl start NetworkManager")
 
 def list_devices():
-	print("thinking...")
+	print(">> thinking...")
 	cmd = "hostname -I"
 	ip = os.popen(cmd).read().split(' ')[0]
 	os.system("sudo nmap "+ip+"/24 | grep MAC")
@@ -40,6 +43,8 @@ while(response != "quit" and response != "q"):
     print("2) Show network data usage for your computer.")
     print("3) Show active network ports for your computer.")
     print("4) Find the path time for a website.")
+    print("5) Stop all networking.")
+    print("6) Start all networking.")
     print("\nUse CTRL+C or type 'quit' to exit.")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     response = input("Choose an option from the list above:\n")
@@ -58,9 +63,15 @@ while(response != "quit" and response != "q"):
 
     if response == "4":
         path_time_to_website()
-    
+
+    if response == "5":
+        shut_down_all_networking()
+
+    if response == "6":
+        start_up_all_networking()
+
     if response == "q" or response == "quit":
-        print("exiting...")
+        print(">> exiting...")
     else:
         print("Please enter a valid command.\n")
 
